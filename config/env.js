c0e9458d626f3ad76d1fcbaaf1b6dae03811d8b5
Junen <liujun.yang@molbase.com>
2020-07-31 09:04:54 +0800

    update env config

--- config/env.js
+++ config/env.js
@@ -4,21 +4,16 @@ module.exports = {
     start: {
         NODE_ENV: '"development"',
         ENV_HOST: '"dev"',
-        BASE_API: '"/api"'
+        BASE_API: '"/api"',
     },
-    test_ta: {
+    test: {
         NODE_ENV: '"production"',
         ENV_HOST: '"ta"',
-        BASE_API: '"http://apidev.molbase.cn"'
-    },
-    test_taa: {
-        NODE_ENV: '"production"',
-        ENV_HOST: '"ta"',
-        BASE_API: '"http://tc.api.saas.molbase.org"'
+        BASE_API: '""',
     },
     prod: {
         NODE_ENV: '"production"',
         ENV_HOST: '""',
-        BASE_API: '"http://api.saas.molbase.org"'
-    }
+        BASE_API: '""',
+    },
 };
